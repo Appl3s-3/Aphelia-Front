20d6ce3d3a692ecebbefac7ec85b5b87fbf46219
I_Agreed <48776053+I-Agreed@users.noreply.github.com>
2022-02-28 16:30:37 +1100

    added more navbar functionality

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -2,11 +2,9 @@
 </script>
 
 <template>
-    <!-- Appl3s to I_Agreed: I think <nav> is the correct tag to use, since technically there'd be a <header> in the <nav>, but that also doesn't make sense -->
-        <!-- TODO: change links to vue links -->
     <div id="navbar">
         <nav id="navbar-nav">
-            <router-link to="home" class="nav-button" id="nav-home">
+            <router-link to="home" class="nav-button nav-logo" id="nav-home">
                 <img src="../assets/logo.svg" id="logo">
             </router-link>
 
@@ -44,9 +42,40 @@
 <style scoped>
 .nav-button {
     display: block;
+    margin: 10px;
+    border-radius: 5px;
+
+    text-align: center;
+    color: #2c3e50;
+    background-color: #a52a2a;
+
+    transition: 0.2s;
+}
+
+.nav-button:hover {
+    background-color: #921e1e;
+}
+
+.nav-button:active {
+    background-color: #801515;
+}
+
+.nav-logo {
+    width: 70px;
+    height: 70px;
+    margin-left: 25px;
+    margin-right: 25px;
+
+    border-radius: 50%;
+}
+
+#navbar {
+    width: 120px;
+    background-color: #e96f6f;
 }
 
 #logo {
+    margin: 10px;
     width: 50px;
     height: 50px;
 }
