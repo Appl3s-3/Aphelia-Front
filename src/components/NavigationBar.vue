befb76c79c5e32025ef1d7f25bc0fd1ecf61ced1
I-Agreed <brendan.alcorn@outlook.com>
2022-02-28 11:06:39 +1100

    added buttons to nav bar

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -4,13 +4,41 @@
 <template>
     <!-- Appl3s to I_Agreed: I think <nav> is the correct tag to use, since technically there'd be a <header> in the <nav>, but that also doesn't make sense -->
     <header>
-        <img src="../assets/logo.svg">
+        <nav>
+            <a href="dashboard.vue">
+                <img src="../assets/logo.svg">
+            </a>
 
-        <a href="settings.vue">
-            Settings
-        </a>
+            <a href="Notifications.vue">
+                Notifications
+            </a>
+
+            <a href="Loans.vue">
+                Loans
+            </a>
+
+            <a href="Items.vue">
+                Items
+            </a>
+
+            <a href="Archetypes.vue">
+                Archetypes
+            </a>
+
+            <a href="Roles.vue">
+                Roles
+            </a>
+
+            <a href="Users.vue">
+                Users
+            </a>
+
+            <a href="Settings.vue">
+                Settings
+            </a>
+        </nav>
     </header>
 </template>
 
 <style scoped>
-</style>
+</style>https://github.com/Appl3s-3/apheleia
