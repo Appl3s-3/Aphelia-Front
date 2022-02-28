2d1b7347bb0a0e7f60e7b6cd6d291288a7b7192b
I-Agreed <brendan.alcorn@outlook.com>
2022-02-28 11:10:29 +1100

    added classes and ids to navbar

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -3,37 +3,38 @@
 
 <template>
     <!-- Appl3s to I_Agreed: I think <nav> is the correct tag to use, since technically there'd be a <header> in the <nav>, but that also doesn't make sense -->
+        <!-- TODO: change links to vue links -->
     <header>
-        <nav>
-            <a href="dashboard.vue">
+        <nav id="navbar">
+            <a href="dashboard.vue" class="nav-button" id="nav-dashboard">
                 <img src="../assets/logo.svg">
             </a>
 
-            <a href="Notifications.vue">
+            <a href="Notifications.vue" class="nav-button" id="nav-notifications">
                 Notifications
             </a>
 
-            <a href="Loans.vue">
+            <a href="Loans.vue" class="nav-button" id="nav-loans">
                 Loans
             </a>
 
-            <a href="Items.vue">
+            <a href="Items.vue" class="nav-button" id="nav-items">
                 Items
             </a>
 
-            <a href="Archetypes.vue">
+            <a href="Archetypes.vue" class="nav-button" id="nav-archetypes">
                 Archetypes
             </a>
 
-            <a href="Roles.vue">
+            <a href="Roles.vue" class="nav-button" id="nav-roles">
                 Roles
             </a>
 
-            <a href="Users.vue">
+            <a href="Users.vue" class="nav-button" id="nav-users">
                 Users
             </a>
 
-            <a href="Settings.vue">
+            <a href="Settings.vue" class="nav-button" id="nav-settings">
                 Settings
             </a>
         </nav>
