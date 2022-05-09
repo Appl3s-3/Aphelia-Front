8d06dc2b3fd6a79e87eb72533ee8c3814f1b351b
I_Agreed <48776053+I-Agreed@users.noreply.github.com>
2022-05-10 09:26:16 +1000

    this should work hopefully

--- src/scripts/auth.js
+++ src/scripts/auth.js
@@ -1,4 +1,6 @@
 import "https"
+import "querystring"
+import { stringify } from "querystring";
 
 const authConfig = {
     auth_uri: "https://student.sbhs.net.au/api/authorize",
@@ -11,23 +13,58 @@ const clientConfig = {
     scope: "all-ro"
 }
 
+function create_code_challenge() {
+    var out = "";
+    var chars = "abcdefghijklmnopqrstuvwxzABCDEFGHIKLMNOPQRSTUVWXYZ1234567890";
+    var len = 10;
+    for (var i = 0; i < len; i++) {
+        out += chars[Math.floor(Math.random() * 62)];
+    }
+
+    var digest = await crypto.subtle.digest("SHA-256", new TextEncoder().encode(out));
+
+    return [out, window.btoa(String.fromCharCode(...new Uint8Array(digest))).replace(/=/g, '').replace(/\+/g, '-').replace(/\//g, '_')];
+}
+
+function gen_state() {
+    var out = "";
+    var chars = "abcdefghijklmnopqrstuvwxzABCDEFGHIKLMNOPQRSTUVWXYZ";
+    var len = 10;
+    for (var i = 0; i < len; i++) {
+        out += chars[Math.floor(Math.random() * 52)];
+    }
+    return out;
+}
+
+
 export function handle_code(params) {
+    // check state
     let state = params.query.state;
-    if (state == "login") { // Get token
+    if (state != localStorage.getItem("authState")) {
+        console.log("bad state")
+    } else { // good state
         let code = params.query.code;
         get_token(code);
-    } else if (state == "token") { // Save tokens
+        /*
         let accessToken = params.query.access_token;
         let refreshToken = params.query.refresh_token;
         // store tokens in localStorage
         window.localStorage.setItem("accessToken", accessToken);
         window.localStorage.setItem("refreshToken", refreshToken);
+        */
     }
 }
 
 export async function get_token(code) {
     // With help from https://github.com/mintcarrotkeys/generic-bells/blob/main/src/apiFetcher.js
-    const body = `grant_type=authorization_code&redirect_uri=${clientConfig.redirect_uri}&client_id=${clientConfig.client_id}&code=${code}`
+    //const body = `grant_type=authorization_code&redirect_uri=${clientConfig.redirect_uri}&client_id=${clientConfig.client_id}&code=${code}`
+    const body = stringify({
+        code_verifier: window.sessionStorage.getItem("codeVerifier"),
+        grant_type: "authorization_code",
+        redirect_uri: clientConfig.redirect_uri,
+        client_id: clientConfig.client_id,
+        code: code,
+    })
     let response = await fetch(authConfig.token_uri, {
         method: "POST",
         headers: {"Content-type": "application/x-www-form-urlencoded; charset=UTF-8"},
@@ -40,10 +77,23 @@ export async function get_token(code) {
 }
 
 export function login() {
+    var [codeVerifier, codeChallenge] = create_code_challenge();
+    window.sessionStorage.setItem("codeVerifier", codeVerifier);
     // Construct address from auth_uri
     // Gets keys from the clientConfig, maps them to a string: "key=value" then joins all the strings with "&"
     // Then appends the state
-    var uri = `${authConfig["auth_uri"]}?${Object.keys(clientConfig).map(x => `${x}=${clientConfig[x]}`).join("&")}&response_type=code&state=${"login"}`;
+    var state = gen_state();
+    localStorage.setItem("authState", state);
+    //var uri = `${authConfig["auth_uri"]}?${Object.keys(clientConfig).map(x => `${x}=${clientConfig[x]}`).join("&")}&response_type=code&state=${state}`;
+    var uri = authConfig["auth_uri"] + stringify({
+        client_id: clientConfig.client_id,
+        redirect_uri: clientConfig.redirect_uri,
+        scope: clientConfig.scope,
+        response_type: "code",
+        state: state,
+        code_challenge_method: "S256",
+        code_challenge: codeChallenge,
+    });
     // Redirect to address
     location.href = uri;
 }
