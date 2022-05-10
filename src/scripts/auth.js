2d0e0fa899ee87a0440c4b9ad02e32de4087d5cc
I_Agreed <48776053+I-Agreed@users.noreply.github.com>
2022-05-11 09:51:48 +1000

    added log

--- src/scripts/auth.js
+++ src/scripts/auth.js
@@ -92,6 +92,7 @@ export async function refresh_token() {
         console.log("response:");
         console.log(tokens);
         localStorage.setItem("accessToken", tokens.access_token);
+        console.log((new Date(Date.now() + (response.expires_in - 5)*1000)).toString());
         localStorage.setItem("accessTokenExpiry", (new Date(Date.now() + (response.expires_in - 5)*1000)).toString()); // creates date now + 1h - 5 seconds
         localStorage.setItem("refreshToken", tokens.refresh_token);
     }
