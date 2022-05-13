d0b370fe8a7df0e0ae2ad1b7d6605bae2c81539e
I_Agreed <48776053+I-Agreed@users.noreply.github.com>
2022-05-13 14:37:10 +1000

    added json to headers

--- src/scripts/apiRequester.js
+++ src/scripts/apiRequester.js
@@ -6,7 +6,9 @@ async function get_user_info() {
     if (accessToken !== null && accessTokenExpiry !== null) { // Token exists
         if (new Date(Date.parse(accessTokenExpiry)) > new Date(Date.now())) { // Token is still valid
             let response = false;
-            await fetch(uri, {headers: {'Authorization': accessToken}}
+            await fetch(uri, {headers: {'Authorization': accessToken,
+                                        'Content-Type': 'application/json',
+                                        'Accept': 'application/json'}}
                 ).then(r => response=r).catch(e => console.log(e));
             return response.json()
         }
