1436f2c06e3c288a292c106511e3c097337d616f
I_Agreed <48776053+I-Agreed@users.noreply.github.com>
2022-08-31 15:06:10 +1000

    added Accept to header

--- src/scripts/backendApi.js
+++ src/scripts/backendApi.js
@@ -3,7 +3,9 @@ const uri = "0.0.0.1:8080"
 export async function get_items() {
     let response = false;
     await fetch(uri + "/items", {method: "GET",
-        headers: {'Authorization': "Bearer " + sessionStorage["userId"]}} // replace with access token later
+        headers: {'Authorization': "Bearer " + sessionStorage["userId"], // replace with access token later
+        'Accept': 'application/json'}
+        }
         ).then(r => response=r).catch(e => console.log(e));
     let data = response.json();
 }
